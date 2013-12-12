0056a4a3bfb6826f371ffd444fe48f3bde927904
metameta <metameta.git@gmail.com>
2013-12-12 23:48:14 +0900

    Version 1.3.1.0 自動更新

--- sengokuixa-meta.user.js
+++ sengokuixa-meta.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           sengokuixa-meta
 // @description    戦国IXAを変態させるツール
-// @version        1.3.0.16
+// @version        1.3.1.0
 // @namespace      sengokuixa-meta
 // @include        http://*.sengokuixa.jp/*
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
